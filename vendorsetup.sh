e34f49385499e6939ce1a82679070bdb1e08d688
Paul Keith <javelinanddart@gmail.com>
2015-07-20 12:06:26 -0500

    Broke it
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo candy5_t769-user
-add_lunch_combo candy5_t769-userdebug
-add_lunch_combo candy5_t769-eng
+add_lunch_combo broken_t769-user
+add_lunch_combo broken_t769-userdebug
+add_lunch_combo broken_t769-eng
